4459d45bea137cd45847a165d3d0a6ef8be251b3
rickey-c <1604120019@qq.com>
2024-12-08 19:51:41 +0800

    feat(Gateway): Gateway通过Nacos做注册中心实现了负载均衡的路由转发
    Signed-off-by: rickey-c <1604120019@qq.com>

--- qiapi-interface/src/main/java/com/rickey/apiInterface/QiApiInterfaceInvokeApplication.java
+++ qiapi-interface/src/main/java/com/rickey/apiInterface/QiApiInterfaceInvokeApplication.java
@@ -3,12 +3,14 @@ package com.rickey.apiInterface;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * QiApi 模拟接口入口类
  */
 @SpringBootApplication
 @MapperScan("com.rickey.apiInterface.mapper")
+@EnableDiscoveryClient
 public class QiApiInterfaceInvokeApplication {
 
     public static void main(String[] args) {
