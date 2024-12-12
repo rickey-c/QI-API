df27cce7dd7cef70da89fa357c343683c01a90f8
rickey-c <1604120019@qq.com>
2024-12-12 20:59:53 +0800

    fix((interfaceInvoke)): 引入新的pom
    Signed-off-by: rickey-c <1604120019@qq.com>

--- qiapi-interface/src/main/java/com/rickey/apiInterface/sentinel/FallbackService.java
+++ qiapi-interface/src/main/java/com/rickey/apiInterface/sentinel/FallbackService.java
@@ -1,6 +1,6 @@
 package com.rickey.apiInterface.sentinel;
 
-import com.rickey.clientSDK.model.User;
+import com.rickey.common.model.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
